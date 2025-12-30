d0464330a7dd3371c2ed6825d0aa3898e02f8063
shdewz <shdewz@gmail.com>
2025-12-30 03:29:10 +0200

    Add express.urlencoded()

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import authRouter from '@routes/auth/index.js';
 import healthRoutes from '@routes/health.js';
+import registrationRoutes from '@routes/registration.js';
 
 import { env } from '@/config/env.js';
 
@@ -11,8 +12,9 @@ const PORT = env.PORT || 4000;
 
 const app = express();
 app.use(helmet());
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 if (env.IS_PROD) app.set('trust proxy', 1);
 else {
@@ -38,6 +40,7 @@ else {
 
 app.use('/auth', authRouter);
 app.use('/health', healthRoutes);
+app.use('/', registrationRoutes);
 
 app.use((_req, res) => res.status(404).send('Not found'));
 
